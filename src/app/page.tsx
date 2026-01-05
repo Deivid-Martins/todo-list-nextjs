bf2b8380f0ed9dff4fa876615498a0d61a6891b1
Deivid-Martins <deividrocha2005@gmail.com>
2026-01-05 10:29:11 -0300

    feat: created initial form to add new tasks in the to do list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+
 export default function Home() {
-  return <div>Deivid</div>;
+  return (
+    <main className="w-full h-screen bg-gray-100 flex justify-center items-center">
+      <Card className="w-lg p-4">
+        <div className="flex gap-2">
+          <Input placeholder="Add new task" />
+          <Button className="cursor-pointer" variant="outline">
+            <Plus />
+            Register
+          </Button>
+        </div>
+
+        <Separator />
+      </Card>
+    </main>
+  );
 }
