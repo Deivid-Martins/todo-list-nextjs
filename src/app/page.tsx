c48092393547e558f082342c89a27ef69db54010
Deivid-Martins <deividrocha2005@gmail.com>
2026-01-05 11:04:47 -0300

    feat: created initial style on tasks in to do list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Check, List, Plus, TriangleAlert } from "lucide-react";
+import {
+  Check,
+  List,
+  Plus,
+  SquarePen,
+  Trash,
+  TriangleAlert,
+} from "lucide-react";
 
 export default function Home() {
   return (
@@ -20,18 +27,29 @@ export default function Home() {
         <CardContent>
           <Separator className="mb-4" />
           <div className="flex gap-2">
-            <Badge className="cursor-pointer">
+            <Badge className="cursor-pointer" variant="default">
               <List />
               All
             </Badge>
-            <Badge className="cursor-pointer">
+            <Badge className="cursor-pointer" variant="outline">
               <TriangleAlert />
               Incomplete
             </Badge>
-            <Badge className="cursor-pointer">
+            <Badge className="cursor-pointer" variant="outline">
               <Check />
               Completed
             </Badge>
+          </div>
+
+          <div className="mt-4 border-b">
+            <div className=" h-14 flex justify-between items-center border-t">
+              <div className="w-1 h-full bg-green-300"></div>
+              <p className="flex-1 px-2 text-sm">Study React</p>
+              <div className="flex gap-2 items-center">
+                <SquarePen size={16} className="cursor-pointer" />
+                <Trash size={16} className="cursor-pointer" />
+              </div>
+            </div>
           </div>
         </CardContent>
       </Card>
